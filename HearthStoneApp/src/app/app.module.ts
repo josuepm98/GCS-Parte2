a083576de71ab2d05158e3635a4d88146b9e501e
Josué Perea <josuepm98@gmail.com>
2022-05-18 17:04:34 +0200

    refresh

--- HearthStoneApp/src/app/app.module.ts
+++ HearthStoneApp/src/app/app.module.ts
@@ -15,4 +15,5 @@ import { AppComponent } from './app.component';
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
+
 export class AppModule {}
